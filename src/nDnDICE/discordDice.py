6f06601fcbb534b7ec709415f790fe346d6278ed
Deize <morisi3160@gmail.com>
2023-12-17 19:57:35 +0900

    [update]token保存変更

--- src/nDnDICE/discordDice.py
+++ src/nDnDICE/discordDice.py
@@ -1,3 +1,5 @@
+import os
+
 import discord
 
 from .nDnDICE import nDn
@@ -6,17 +8,18 @@ client = discord.Client()
 
 
 @client.event
-async def on_ready():
+async def on_ready() -> None:
     print("Botを起動しました。")
 
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message) -> None:
     msg = message.content
     result = nDn(msg)
     if result is not None:
         await client.send_message(message.channel, result)
 
 
-# ここにbotのアクセストークンを入力
-client.run("***")
+# discord botのトークンは環境変数DICE_BOT_TOKENに保存
+def bot_run() -> None:
+    client.run(os.getenv("DICE_BOT_TOKEN"))
