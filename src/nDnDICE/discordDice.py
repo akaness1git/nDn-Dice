5a42a110b2e51074226354fb690cc4c7d0dae203
Deize <morisi3160@gmail.com>
2023-12-17 18:28:17 +0900

    [update]パッケージ対応

--- src/nDnDICE/discordDice.py
+++ src/nDnDICE/discordDice.py
@@ -1,18 +1,22 @@
 import discord
-import nDnDICE
+
+from .nDnDICE import nDn
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    print('Botを起動しました。')
+    print("Botを起動しました。")
+
 
 @client.event
 async def on_message(message):
     msg = message.content
-    result = nDnDICE.nDn(msg)
+    result = nDn(msg)
     if result is not None:
         await client.send_message(message.channel, result)
-    
-#ここにbotのアクセストークンを入力
-client.run('***')
+
+
+# ここにbotのアクセストークンを入力
+client.run("***")
